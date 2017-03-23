5a3782c2485784bdb419818cd746886f34bc60d6
dariukas <dariui.miliauskui@gmail.com>
2017-03-23 17:29:41 +0200

    Libraries Commit

---
+++ iOSParty/ViewControllers/ServersViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ServersViewController.swift
+//  iOSParty
+//
+//  Created by Darius Miliauskas on 23/03/2017.
+//  Copyright © 2017 Darius Miliauskas. All rights reserved.
+//
+
+import UIKit
+
+class ServersViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UserModel.getToken()
+           {
+            var sm = ServerModel()
+            sm.getServers() {model in
+                sm = model.first!
+                print(sm)
+           }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Backend methods
+    
+
+
+}
+
